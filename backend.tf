64e83b45be19d397623afba457b565e5e08fc01d
harikagouth <111902730+harikagouth@users.noreply.github.com>
2022-12-02 19:15:51 -0500

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 0.12.2"
+
+  backend "s3" {
+    region         = "ca-central-1"
+    bucket         = "aws-lab-eks-cluster-dev-terraform-states"
+    key            = "lab.tfstate"
+    dynamodb_table = "aws-lab-eks-cluster-dev-terraform-states-lock"
+    profile        = "cofomo"
+    role_arn       = ""
+    encrypt        = "true"
+  }
+}
+
